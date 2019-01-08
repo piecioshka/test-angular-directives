9eac352f56fce03172c311661a17f6a3687b08f7
Piotr Kowalski <piecioshka@gmail.com>
2019-01-08 11:55:38 +0100

    Create directive and deploy task

---
+++ src/app/directives/super-border.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SuperBorderDirective } from './super-border.directive';
+
+describe('SuperBorderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SuperBorderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
